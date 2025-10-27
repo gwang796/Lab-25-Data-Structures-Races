43fadcf16ead37eb301e6c39a618264f40805e8f
Guo An Wang <you@example.com>
2025-10-26 20:08:02 -0700

    third commit

--- Lab 25: Data Structures Races/main.cpp
+++ Lab 25: Data Structures Races/main.cpp
@@ -15,6 +15,8 @@
 using namespace std;
 using namespace std::chrono;
 const string txt = "codes.txt";
+const int width1 = 8;
+const int width2 = 12;
 
 int main() {
     vector<string> vect;
@@ -29,14 +31,35 @@ int main() {
         vect.push_back(code);
     }
     auto end = high_resolution_clock::now();
-    auto duration1 = duration_cast<milliseconds>(end - start);
+    auto duration1 = duration_cast<microseconds>(end - start);
     
     inputFile.clear(); //clear inputFile
     inputFile.seekg(0); //move back to start of file
     
     start = high_resolution_clock::now();
+    while(inputFile >> code)
+    {
+        lst.push_back(code);
+    }
+    end = high_resolution_clock::now();
+    auto duration2 = duration_cast<microseconds>(end - start);
+    
+    inputFile.clear(); //clear inputFile
+    inputFile.seekg(0); //move back to start of file
+    
+    start = high_resolution_clock::now();
+    while(inputFile >> code)
+    {
+        st.insert(code);
+    }
+    end = high_resolution_clock::now();
+    auto duration3 = duration_cast<microseconds>(end - start);
+    
+    cout << "Operation" << setw(width1) << "Vector" << setw(width1) << "List" << setw(width1) << "Set" << endl;
+    cout << "Read" << setw(width2) << duration1.count() << setw(width1) << duration2.count() << setw(width2) << duration3.count() << endl;
     
     
+
     
 }
 
