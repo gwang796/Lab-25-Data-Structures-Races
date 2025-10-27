2e24e7f174a540464f382741bd22885b5d400708
Guo An Wang <you@example.com>
2025-10-26 19:47:03 -0700

    Initial commit

---
+++ Lab 25: Data Structures Races/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Lab 25: Data Structures Races
+//
+//  Created by Guo An Wang on 10/26/25.
+//
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+    
+
+    return 0;
+}
+
+/* syntax examples:
+auto start = high_resolution_clock::now()
+auto end = high_resolution_clock::now()
+auto duration = duration_cast<milliseconds>(end - start)
+duration.count() references elapsed milliseconds
+*/
