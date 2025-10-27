12ac45951ace0185f526d4f85501d6a8f312619c
Guo An Wang <you@example.com>
2025-10-26 19:58:26 -0700

    second commit

--- Lab 25: Data Structures Races/main.cpp
+++ Lab 25: Data Structures Races/main.cpp
@@ -6,14 +6,38 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <set>
 #include <chrono>
+#include <algorithm>
 using namespace std;
 using namespace std::chrono;
+const string txt = "codes.txt";
 
 int main() {
+    vector<string> vect;
+    list<string> lst;
+    set<string> st;
+    ifstream inputFile(txt);
+    string code;
     
-
-    return 0;
+    auto start = high_resolution_clock::now();
+    while(inputFile >> code)
+    {
+        vect.push_back(code);
+    }
+    auto end = high_resolution_clock::now();
+    auto duration1 = duration_cast<milliseconds>(end - start);
+    
+    inputFile.clear(); //clear inputFile
+    inputFile.seekg(0); //move back to start of file
+    
+    start = high_resolution_clock::now();
+    
+    
+    
 }
 
 /* syntax examples:
